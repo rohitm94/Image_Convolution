a10a7383f58f9b4f1d7e24979adbb23ec09f5f22
rohitm94 <rohit11094@gmail.com>
2019-10-14 22:05:00 -0400

    base code second commit

--- base_code/base_code.cpp
+++ base_code/base_code.cpp
@@ -1,1 +1,25 @@
-# Image_Convolution
+// Image_Convolution base code
+
+#include <iostream>
+#include <immintrin.h>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+int main(int argc, char *argv[])
+{
+
+    chrono::time_point<system_clock> clock_start, clock_end;
+    chrono::duration<double> elapsed_time;
+
+    int thread_count, thread_id;
+
+#pragma omp parallel
+    {
+    }
+
+    cout << endl;
+    return 0;
+}
