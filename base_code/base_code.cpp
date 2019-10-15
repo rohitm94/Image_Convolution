f5b84dced08fdf60bf4b00f5b7ff16bae4099bd4
rohitm94 <rohit11094@gmail.com>
2019-10-15 04:17:39 -0400

    non parallel base code

--- base_code/base_code.cpp
+++ base_code/base_code.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <vector>
 #include <stdlib.h>
+#include <math.h>
 
 using namespace std;
 using namespace chrono;
@@ -19,23 +20,62 @@ int main(int argc, char *argv[])
     //int k = atoi(argv[1]);
     //int n = atoi(argv[2]);
     //int m = atoi(argv[3]);
-    vector<vector<int>> image_size = {{1024, 768}, {2048, 2048}, {8192, 8192}, {4194304, 768}, {16777216, 768}};
+    vector<vector<long long int>> image_size = {{1024, 768}}; //,{2048, 2048}, {8192, 8192}, {4194304, 768}, {16777216, 768}};
 
     for (int i = 0; i < kernel_size.size(); i++)
     {
+        int kernel_data[kernel_size[i]][kernel_size[i]];
 
-        vector<vector<int>> kernel_data(kernel_size[i], vector<int>(kernel_size[i], 1));
+        for (int g = 0; g < kernel_size[i]; g++)
+        {
+            for (int h = 0; h < kernel_size[i]; h++)
+            {
+                kernel_data[g][h] = (rand() % 3) + (-1);
+            }
+        }
 
         for (int j = 0; j < image_size.size(); j++)
         {
-            vector<vector<int>> image_data(image_size[j][0], vector<int>(image_size[j][1], rand() % 256));
-            for (int a = 0; a < kernel_data.size(); a++)
+            //vector<vector<int>> image_data(image_size[j][0], vector<int>(image_size[j][1], rand() % 255));
+            int image_data[image_size[j][0]][image_size[j][1]];
+
+            for (int g = 0; g < image_size[j][0]; g++)
             {
-                for (int b = 0; b < kernel_data.size(); b++)
+                for (int h = 0; h < image_size[j][1]; h++)
                 {
-                    cout << kernel_data[a][b] << endl;
+                    image_data[g][h] = (rand() % 256);
                 }
             }
+            clock_start = system_clock::now();
+            for (int k = floor(kernel_size[i] / 2); k < (image_size[j][0] - int(floor(kernel_size[i] / 2))); ++k)
+            {
+                for (int l = floor(kernel_size[i] / 2); l < (image_size[j][1] - int(floor(kernel_size[i] / 2))); ++l)
+                {
+                    long int conv_out = 0;
+                    for (int a = 0; a < kernel_size[i]; ++a)
+                    {
+
+                        for (int b = 0; b < kernel_size[i]; ++b)
+                        {
+                            conv_out += kernel_data[a][b] * image_data[k - int(floor(kernel_size[i] / 2)) + a][l - int(floor(kernel_size[i] / 2)) + b];
+                        }
+                    }
+                    image_data[k][l] = conv_out;
+                }
+            }
+            clock_end = system_clock::now();
+
+            elapsed_time = clock_end - clock_start;
+            double run_time = elapsed_time.count();
+            /*for (int y = 0; y < image_size[j][0]; y++)
+        		{
+				for (int z = 0; z < image_size[j][1]; z++)
+                			{
+                    				cout << image_data[y][z]<<" ";
+                			}
+                		cout<< endl;
+        		}*/
+            cout << "Time taken for convolution of " << image_size[j][0] << "*" << image_size[j][1] << " by kernel size of " << kernel_size[i] << "is" << run_time << endl;
         }
     }
     // vector<vector<int>> kernel_data(k, vector<int> (k, 1));
