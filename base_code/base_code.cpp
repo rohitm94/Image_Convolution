14b774a51b412b85ac9a5952cef3f6c0b5b6faf9
rohitm94 <rohit11094@gmail.com>
2019-10-15 00:25:55 -0400

    checking base code

--- base_code/base_code.cpp
+++ base_code/base_code.cpp
@@ -4,22 +4,43 @@
 #include <immintrin.h>
 #include <omp.h>
 #include <chrono>
+#include <vector>
+#include <stdlib.h>
 
 using namespace std;
 using namespace chrono;
 
 int main(int argc, char *argv[])
 {
-
     chrono::time_point<system_clock> clock_start, clock_end;
     chrono::duration<double> elapsed_time;
 
-    int thread_count, thread_id;
+    vector<int> kernel_size = {3, 5, 7, 9, 11, 13, 15};
+    //int k = atoi(argv[1]);
+    //int n = atoi(argv[2]);
+    //int m = atoi(argv[3]);
+    vector<vector<int>> image_size = {{1024, 768}, {2048, 2048}, {8192, 8192}, {4194304, 768}, {16777216, 768}};
 
-#pragma omp parallel
+    for (int i = 0; i < kernel_size.size(); i++)
     {
-    }
 
-    cout << endl;
+        vector<vector<int>> kernel_data(kernel_size[i], vector<int>(kernel_size[i], 1));
+
+        for (int j = 0; j < image_size.size(); j++)
+        {
+            vector<vector<int>> image_data(image_size[j][0], vector<int>(image_size[j][1], rand() % 256));
+            for (int a = 0; a < kernel_data.size(); a++)
+            {
+                for (int b = 0; b < kernel_data.size(); b++)
+                {
+                    cout << kernel_data[a][b] << endl;
+                }
+            }
+        }
+    }
+    // vector<vector<int>> kernel_data(k, vector<int> (k, 1));
+
+    // vector<vector<int>> image_data(n, vector<int> (m, rand()%256));
+
     return 0;
 }
