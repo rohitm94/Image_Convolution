f2272ca59e5e657f6748a549b1d24d605688071d
rohitm94 <rohit11094@gmail.com>
2019-10-16 00:44:19 -0400

    parallel performance test base code

--- base_code/base_code.cpp
+++ base_code/base_code.cpp
@@ -62,18 +62,25 @@ int main(int argc, char *argv[])
                 new_image_data[a] = (int *)malloc(image_size[j][1] * sizeof(int));
 
             clock_start = system_clock::now();
-            //#pragma omp parallel for
+
             for (int k = floor(kernel_size[i] / 2); k < (image_size[j][0] - int(floor(kernel_size[i] / 2))); ++k)
             {
                 for (int l = floor(kernel_size[i] / 2); l < (image_size[j][1] - int(floor(kernel_size[i] / 2))); ++l)
                 {
                     long int conv_out = 0;
-                    for (int a = 0; a < kernel_size[i]; ++a)
+#pragma omp parallel
                     {
-
-                        for (int b = 0; b < kernel_size[i]; ++b)
+#pragma omp for
+                        for (int a = 0; a < kernel_size[i]; ++a)
                         {
-                            conv_out += kernel_data[a][b] * image_data[k - int(floor(kernel_size[i] / 2)) + a][l - int(floor(kernel_size[i] / 2)) + b];
+#pragma omp parallel shared(conv_out, a, k, l)
+                            {
+#pragma omp for
+                                for (int b = 0; b < kernel_size[i]; ++b)
+                                {
+                                    conv_out += kernel_data[a][b] * image_data[k - int(floor(kernel_size[i] / 2)) + a][l - int(floor(kernel_size[i] / 2)) + b];
+                                }
+                            }
                         }
                     }
                     new_image_data[k][l] = conv_out;
